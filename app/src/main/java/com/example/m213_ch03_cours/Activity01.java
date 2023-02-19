e39954685fd6fd96d36cabb1b1e517f57ad44eac
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-02-19 19:29:31 +0000

    Initial commit

---
+++ app/src/main/java/com/example/m213_ch03_cours/Activity01.java
@@ -0,0 +1,14 @@
+package com.example.m213_ch03_cours;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_01);
+    }
+}
